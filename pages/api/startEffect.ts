e1c7bcfced8259ad88cba6a320ecd9eb4dd1853d
Shuffle21 <96444816+Shuffle21@users.noreply.github.com>
2022-06-29 14:34:04 +0300

    API Route startEffect response change
    Returns now a boolean instead of a string

--- pages/api/startEffect.ts
+++ pages/api/startEffect.ts
@@ -2,14 +2,15 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { ToggleEffect } from './../../services/effect';
 
 type Data = {
-  status: string;
+  status: boolean;
 };
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     ToggleEffect(req.body.effect, req.body.status);
+  res.status(200).json({ status: true });
   } catch (ex) {
     console.log(ex);
+    res.status(200).json({ status: false });
   }
-  res.status(200).json({ status: 'Done!' });
 };
